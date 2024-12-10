4f8216443598a445c47c62b1066c08ac7e71293c
Jeferson Viana <jefersonv28@gmail.com>
2024-12-10 15:52:50 -0300

    Update LivrosDoados.jsx

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -1,1 +1,20 @@
+import Header from './../../components/header/Header';
+import Footer from './../../components/footer/Footer';
+import s from './livrosDoados.module.scss';
+import img from '../../assets/livroDoado.png'
 
+const QueroDoar = () => {
+  return (
+    <>
+    <Header />
+    <main className={s.livrosDoadosMain}>
+      <section className={s.livrosDoadosSection}>
+        <img src={img} alt="livro doado" className={s.livrosDoadosImg} />
+      </section>
+    </main>
+    <Footer />
+    </>
+  );
+};
+
+export default QueroDoar;
